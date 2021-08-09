8f5a571421e7f702a21a370ee9c7dec596b35a3b
Alexis Ferrucci <alexis.ferrucci@b-yond.com>
2021-08-09 18:40:49 -0300

    Cambio para probar merge.

--- proyectos/python-flask/app.py
+++ proyectos/python-flask/app.py
@@ -7,6 +7,7 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
 bind_port = int(os.environ['BIND_PORT'])
+bind_port2 = int(os.environ['BIND_PORT2'])
 
 
 @app.route('/')
@@ -15,6 +16,6 @@ def hello():
     total_hits = redis.get('hits').decode()
     return f'Hello from Redis! I have been seen {total_hits} times.'
 
-
+# hola como les va!!!
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=bind_port)
