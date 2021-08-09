2f84e8f995c11834ab823820e37d6f891c49ab8b
Alexis Ferrucci <alexis.ferrucci@b-yond.com>
2021-08-09 18:44:10 -0300

    Mis cambios que son muy buenos!!!

--- proyectos/python-flask/app.py
+++ proyectos/python-flask/app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 redis = Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
 bind_port = int(os.environ['BIND_PORT'])
 bind_port2 = int(os.environ['BIND_PORT2'])
-
+bind_port3 = int(os.environ['BIND_PORT3'])
 
 @app.route('/')
 def hello():
     redis.incr('hits')
     total_hits = redis.get('hits').decode()
-    return f'Hello from Redis! I have been seen {total_hits} times.'
+    return f'Hello from Redis! I have been seen {total_hits} times!!!'
 
 # hola como les va!!!
 if __name__ == "__main__":
